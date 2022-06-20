f01b02a827cc140e5d8a106e35b99b92b69f2b2d
CleiL <clei.lisboa@gmail.com>
2022-06-20 15:42:11 -0300

    atualização

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -12,7 +12,7 @@ export class LoginService {
 
   url = "http://localhost:3000/login";
 
-  login(usuario: User):Observable<any> {
+  login(usuario: User): Observable<any> {
     return this.httpClient.post(this.url, JSON.stringify(usuario), {
       headers: new HttpHeaders({'Content-Type':"application/json"}),
       observe: "response"
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -13,22 +13,40 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+
   userModel = new User()
   mensagem = ""
 
   receberDados(){
     console.log(this.userModel)
     this.loginService.login(this.userModel).subscribe((response) => {
-      //console.log("response:", response)
-      //console.log("O Status Code é:", response.status)
+      console.log("response:", response)
+      console.log("O Status Code é:", response.status)
       console.log("O tokken é:", response.body.accessToken)
 
       this.mensagem = "bem vindo " + response.body.user.nome
       console.log(this.mensagem)
-    }, (erro) =>{
-      console.log(erro)
+    }, (responseerro) => {
+      console.log("responseerro", responseerro)
+      this.mensagem = responseerro.error
     })
+  }
+
+  onSubmit() {
+    console.log("Modelo:", this.userModel)
+    const lista: string[] = ["select","from","drop","or","having","group","by","insert","exec","\'","\"","*","#"]
+
+    lista.forEach(palavra => {
+      this.mensagem = "dados invalidos" + palavra
+      if(this.userModel.email?.toLocaleLowerCase().includes(palavra)){
+
+      }
+    });
+    this.loginService.login(this.userModel).subscribe( (response) =>{
+      this.mensagem = "login sucedido";
+    }, (erro) => {
+      this.mensagem = erro.error;
+    } )
   }
 
 }
